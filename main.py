0d22507642d1b44856c2d0f19550b1f0d35ff4d4
Sami Barchid <sami.barchid@univ-lille.fr>
2022-02-10 13:48:26 +0100

    lol

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def create_module(args, datamodule: DVSDataModule) -> pl.LightningModule:
     dict_args = vars(args)
     dict_args['in_channels'] = 20 if args.event_representation in ('frames_event', 'frames_time') else 2
     dict_args['in_channels'] = 9 if args.event_representation == "VoxelGrid" else 2
-    dict_args['in_channels'] = 2 if args.event_representation == "frames" else 2
+    dict_args['in_channels'] = 2 if args.event_representation == "frames" else 2 # wtf sami
     dict_args['num_classes'] = datamodule.num_classes
 
     # TODO: you can change the module class here
