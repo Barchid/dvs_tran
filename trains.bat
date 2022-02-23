5d78f79e3d4135e81949e9462d2661bcfa6f68b1
Sami Barchid <sami.barchid@univ-lille.fr>
2022-02-23 16:53:02 +0100

    cifar10dvs done

--- trains.bat
+++ trains.bat
@@ -36,8 +36,27 @@ python main.py --default_root_dir="experiments/bit_encoding" --gpus=1 --event_re
 @REM 99.4
 python main.py --default_root_dir="experiments/voxel_nmnist" --num_workers=8 --gpus=1 --event_representation="VoxelGrid" --timesteps=10 --dataset="n-mnist" --batch_size=1024 --max_epochs=60 --height=224 --width=224 --learning_rate=0.009
 
-@REM 99.3
 python main.py --default_root_dir="experiments/bit_nmnist" --num_workers=8 --gpus=1 --event_representation="bit_encoding" --timesteps=8 --dataset="n-mnist" --batch_size=1024 --max_epochs=60 --height=224 --width=224 --learning_rate=0.0019
 
+@REM 99.4
+python main.py --default_root_dir="experiments/framestime_nmnist" --num_workers=8 --gpus=1 --event_representation="frames_time" --timesteps=10 --dataset="n-mnist" --batch_size=1024 --max_epochs=60 --height=224 --width=224 --learning_rate=0.0019 --mode="lr_find"
 
-python main.py --default_root_dir="experiments/histo_nmnist" --num_workers=8 --gpus=1 --event_representation="histogram" --timesteps=10 --dataset="n-mnist" --batch_size=1024 --max_epochs=60 --height=224 --width=224 --learning_rate=0.0019 --mode="lr_find"
+
+python main.py --default_root_dir="experiments/framestime_cifar10" --num_workers=5 --gpus=1 --event_representation="frames_time" --timesteps=10 --dataset="cifar10-dvs" --batch_size=1024 --max_epochs=60 --height=224 --width=224 --learning_rate=0.001
+python main.py --default_root_dir="experiments/voxel_cifar10" --num_workers=5 --gpus=1 --event_representation="VoxelGrid" --timesteps=10 --dataset="cifar10-dvs" --batch_size=1024 --max_epochs=60 --height=224 --width=224 --learning_rate=0.001
+
+
+@REM 79.55
+python main.py --default_root_dir="experiments/voxel_gesture" --num_workers=2 --gpus=1 --event_representation="VoxelGrid" --timesteps=10 --dataset="dvsgesture" --batch_size=256 --max_epochs=60 --height=224 --width=224 --learning_rate=0.001
+@REM 82.95
+python main.py --default_root_dir="experiments/framestime_gesture" --num_workers=2 --gpus=1 --event_representation="frames_time" --timesteps=10 --dataset="dvsgesture" --batch_size=256 --max_epochs=60 --height=224 --width=224 --learning_rate=0.001
+@REM 90.91
+python main.py --default_root_dir="experiments/histogram_gesture" --num_workers=2 --gpus=1 --event_representation="histogram" --timesteps=10 --dataset="dvsgesture" --batch_size=256 --max_epochs=60 --height=224 --width=224 --learning_rate=0.001
+
+python main.py --default_root_dir="experiments/bit_gesture" --num_workers=2 --gpus=1 --event_representation="bit_encoding" --timesteps=10 --dataset="dvsgesture" --batch_size=256 --max_epochs=60 --height=224 --width=224 --learning_rate=0.001
+
+
+python main.py --default_root_dir="experiments/voxel_gesture" --num_workers=4 --gpus=1 --event_representation="VoxelGrid" --timesteps=10 --dataset="dvsgesture" --batch_size=256 --max_epochs=60 --height=224 --width=224 --learning_rate=0.001
+python main.py --default_root_dir="experiments/framestime_gesture" --num_workers=4 --gpus=1 --event_representation="frames_time" --timesteps=10 --dataset="dvsgesture" --batch_size=256 --max_epochs=60 --height=224 --width=224 --learning_rate=0.001
+python main.py --default_root_dir="experiments/histogram_gesture" --num_workers=4 --gpus=1 --event_representation="histogram" --timesteps=10 --dataset="dvsgesture" --batch_size=256 --max_epochs=60 --height=224 --width=224 --learning_rate=0.001
+python main.py --default_root_dir="experiments/bit_gesture" --num_workers=4 --gpus=1 --event_representation="bit_encoding" --timesteps=10 --dataset="dvsgesture" --batch_size=256 --max_epochs=60 --height=224 --width=224 --learning_rate=0.001
